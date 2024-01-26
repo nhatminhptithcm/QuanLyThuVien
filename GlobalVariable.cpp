4ff476d5b9a2b58c03f5fcf1f7bad30e40d73283
MSI\nhatm <nhatminhptithcm@gmail.com>
2024-01-27 03:57:50 +0700

    Add file

---
+++ GlobalVariable.cpp
@@ -0,0 +1,14 @@
+//
+// Created by nhatm on 1/27/2024.
+//
+
+#include "GlobalVariable.h"
+
+int locate  = 0;
+int index   = 0;
+int nDG  = 0 ;
+bool done = false;
+string tensach = "";
+string masach = "";
+string vitrisach = "";
+char dongthongbao[] = "          Press Any Key to continue !!!";
